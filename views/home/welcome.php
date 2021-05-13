b8dd94f276c602a5135fa2037aa2c704922199f9
Ben Hassan <loftyd@live.co.uk>
2021-05-13 22:12:45 +0100

    new resources

--- views/home/welcome.php
+++ views/home/welcome.php
@@ -5,7 +5,7 @@
 		</div>
 		<div class="col-md-9">
 			<h1>Welcome To Rampage API Documentation!</h1>
-			<p>Welcome to the API Documentation of Rampage which you can use to enhance your experience on Rampage.</p>
+			<p>Welcome to the API Documentation for Rampage which you can use to enhance your experience on Rampage.</p>
 
 			<p>Some resources are publicly accessible and others will require an <strong>Authorization token</strong> supplied in the header of the resource request, this will be made known on each respective resource.</p>		
 		</div>
--- views/sidebar.php
+++ views/sidebar.php
@@ -1,12 +1,19 @@
 <h2>Resources</h1>
 <ul>
 	<li><a data-toggle="collapse" href="#events" class="menu-item"><strong>Events</strong></a>
-		<ul class='collapse' id="events">
+		<ul class="collapse" id="events">
 			<li><a href="/events/shareable_link">/events/shareable_link</a></li>
+			<li><a href="/events/signup">/events/signup</a></li>
+		</ul>
+	</li>
+	<li><a data-toggle="collapse" href="#robots" class="menu-item"><strong>Robots</strong></a>
+		<ul class="collapse" id="robots">
+			<li><a href="/robots/find_by_name">/robots/find_by_name</a></li>
+			<li><a href="/robots/show">/robots/show</a></li>
 		</ul>
 	</li>
 	<li><a data-toggle="collapse" href="#users" class="menu-item"><strong>Users</strong></a>
-		<ul class='collapse' id="users">
+		<ul class="collapse" id="users">
 			<li><a href="/users/show">/users/show</a></li>
 		</ul>
 	</li>
