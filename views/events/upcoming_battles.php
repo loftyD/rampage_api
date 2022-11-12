c689f6d6be1a6303d27de177a69947079b136fa3
Ben Hassan <loftyd@live.co.uk>
2022-11-12 15:40:14 +0000

    Update upcoming_battles.php

--- views/events/upcoming_battles.php
+++ views/events/upcoming_battles.php
@@ -11,13 +11,29 @@ $params = [
 		"required" => true, 
 		"type" => "integer", 
 		"description" => "ID of the event you want the upcoming fights for."
+	],
+	[
+		"property" => "limit",
+		"required" => false, 
+		"type" => "integer", 
+		"description" => "Filters how many upcoming fights should appear. Defaults to 5."
 	]
 ];
 $documentation->resourceDescribe($params);
 
 $response = [
 	"success" =>json_decode('{"currentBattle":"The Rovengers vs Crossblow II","currentBattleName":"Heat 1 Round 1 Eliminator 1","nextBattles":["Heat 1 Round 1 Eliminator 2 : Exhilarator vs Bourbon","Heat 2 Round 1 Eliminator 1 : Inertia B4 vs M.E.T.A","Heat 2 Round 1 Eliminator 2 : Rust in Pieces vs Léim","Heat 3 Round 1 Eliminator 1 : Ion vs Drizzle","Heat 3 Round 1 Eliminator 2 : F.7 vs Sprocket Raccoon"]}'),
-	"fail" => []
+	"fail" => [
+		"on_event_not_provided" => [
+			"error" => "ID must be specified."
+		],
+		"on_event_not_found" => [
+			"error" => "Event could not be found."
+		],
+		"on_limit_specified_but_not_integer" => [
+			"error" => "Limit must be numeric."
+		]
+	]
 ];
 $documentation->resourceResponse($response);
 ?>
